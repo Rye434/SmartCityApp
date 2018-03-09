03a5922a0bd6f6f16ba2b2b99a8f2e5cdc09eeac
Rye434 <grif0193@algonquinlive.com>
2018-03-09 11:05:02 -0500

    Fixed Login Flow Components
    Added and removed components to the login flow to match wireframe. 
    Added strings.

--- src/js/shared/res/strings/StringsEN.js
+++ src/js/shared/res/strings/StringsEN.js
@@ -19,6 +19,7 @@ module.exports = {
     FIELDS_FIRST_NAME:"First name",
     FIELDS_LAST_NAME:"Last name",
     FIELDS_CONFIRM:"Confirm",
+    FIELDS_COUNTRY:"Country",
 
     BUTTONS_VERIFY:"Verify",
     BUTTONS_LOGIN:"Login",
@@ -33,8 +34,15 @@ module.exports = {
     REQUEST_SEGMENT_PERSONAL:"Mine",
     REQUEST_SEGMENT_ACKNOWLEDGED:"Acknowledged",
 
+    PROFILE_PHONE:"Phone Number",
+    PROFILE_EMAIL:"Email",
+    PROFILE_ADDRESS:"Address",
 
+    LOGIN_PHONE:"Phone Number",
+    LOGIN_MESSAGE:"Enter a phone number that can be used to verify your identity with a text message.",
 
+    VERIFICATION_HEADER:"Enter Verification Code",
+    VERIFICATION_MESSAGE:"A text message with a verification code has been sent to your phone number. Enter the code to continue.",
 
 
 }
