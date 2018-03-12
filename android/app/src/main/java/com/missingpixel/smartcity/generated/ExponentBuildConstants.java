9e4a5461a3951bea731896aa18aa868afa038739
Unknown <tout0004@algonquinlive.com>
2018-03-12 14:29:21 -0400

    ejected native-base-theme

--- android/app/src/main/java/com/missingpixel/smartcity/generated/ExponentBuildConstants.java
+++
@@ -1,8 +0,0 @@
-package com.missingpixel.smartcity.generated;
-
-// This file is auto-generated. Please don't rename!
-public class ExponentBuildConstants {
-
-  public static final String DEVELOPMENT_URL = "exp85bcecf637f0416ab7df1d5f01d5204d://10.70.208.218:19000";
-
-}
