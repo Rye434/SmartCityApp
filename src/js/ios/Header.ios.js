8c94821662d41ac92e7e5790fdb4df1c84b6d80e
Rye434 <grif0193@algonquinlive.com>
2018-03-13 11:38:41 -0400

    Layout for PhoneFacebook and RequestListItem
    made layouts

--- src/js/ios/Header.ios.js
+++ src/js/ios/Header.ios.js
@@ -15,7 +15,7 @@ export default class HeaderIos extends Component {
                     <Title>{this.props.title}</Title>
                     </Body>
                     <Right>
-                        <Button primary transparent onPress={this.props.buttonClickRight}>
+                        <Button transparent onPress={this.props.buttonClickRight}>
                             <Text>{this.props.targetTextRight}</Text>
                         </Button>
                     </Right>
